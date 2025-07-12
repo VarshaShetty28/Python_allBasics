84e70309623a73325f30e7b1f95b7d05fad081c9
VarshaShetty28 <varshashetty551@gmail.com>
2025-07-12 14:49:56 +0530

    binary search

---
+++ Full_python_basics/Programs_Imp/searchh.py
@@ -0,0 +1,25 @@
+# function to search in a sorted integer array 
+# if it is sorted we can make it optimized and fast 
+# problem is : search a integer in sorted array :
+def search(num,key):
+    left = 0
+    right = len(num)-1
+   
+    found = 0 
+    while(left <= right):
+        mid = int((left+right)/2)
+        if num[mid] == key:
+            found = 1
+            break #breaking is very imp inside any loop or else it will be keep running
+        elif num[mid] > key :
+            right = mid-1
+        elif num[mid] < key:
+            left = mid+1
+    if found == 1:
+        print(f'Number is found and the location of the number is (index is) : {mid}')
+    else:
+        print('Number is not found')
+
+num = [1, 2, 3, 5, 7, 8, 9, 10, 15, 45]
+key = 1
+search(num,key)
